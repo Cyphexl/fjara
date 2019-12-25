260f4b3ba8b26295cb58e08c060479c34965bcd9
Tzingtao Chow <Cyphexl@outlook.com>
2019-12-26 01:09:53 +0800

    random-color

---
+++ main.js
@@ -0,0 +1,27 @@
+const DARK = "#222222"
+const LIGHT = "#EEEEEE"
+
+const setColor = color => {
+    document.body.style.backgroundColor = color.back
+    document.getElementById("theme-color").setAttribute("content", color.fore)
+    document.getElementById("fjara").setAttribute("fill", color.fore)
+}
+
+const getRandomColor = () => "#" + ((1 << 24) * Math.random() | 0).toString(16)
+
+const getLuma = c => {
+    var c = c.substring(1)
+    var rgb = parseInt(c, 16)
+    var r = (rgb >> 16) & 0xff
+    var g = (rgb >> 8) & 0xff
+    var b = (rgb >> 0) & 0xff
+    console.log(0.2126 * r + 0.7152 * g + 0.0722 * b)
+    return 0.2126 * r + 0.7152 * g + 0.0722 * b
+}
+
+setInterval(() => {
+    let color = {}
+    color.fore = getRandomColor()
+    color.back = getLuma(color.fore) < 100 ? LIGHT : DARK
+    setColor(color)
+}, 1248)
